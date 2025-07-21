8c86d0ecddb5ea029a7551e6475367060069b876
garvjain24 <garvjain2410@gmail.com>
2025-07-21 15:40:24 +0530

    UPDATE

--- netlify/functions/sitemap.js
+++ netlify/functions/sitemap.js
@@ -2,8 +2,8 @@ const admin = require('firebase-admin');
 
 // Add Firebase configuration directly in the function to avoid path issues
 const firebaseConfig = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+  clientEmail: process.env.VITE_FIREBASE_CLIENT_EMAIL,
   privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
 };
 
