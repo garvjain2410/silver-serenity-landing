575c4417fd2b90441952fc239ba3f3b748daa0a9
garvjain24 <garvjain2410@gmail.com>
2025-07-21 15:42:27 +0530

    check

--- netlify/functions/sitemap.js
+++ netlify/functions/sitemap.js
@@ -4,7 +4,7 @@ const admin = require('firebase-admin');
 const firebaseConfig = {
   projectId: process.env.VITE_FIREBASE_PROJECT_ID,
   clientEmail: process.env.VITE_FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  privateKey: process.env.VITE_FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
 };
 
 // Initialize Firebase Admin SDK
